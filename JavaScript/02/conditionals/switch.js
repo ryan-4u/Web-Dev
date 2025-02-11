9b5542388444b4f01812e5a6c227f4139e1ef566
ryan-4u <aaryanofficial64@gmail.com>
2025-02-11 17:36:58 +0530

    switch statement

---
+++ JavaScript/02/conditionals/switch.js
@@ -0,0 +1,14 @@
+let color ="red" ;
+switch(color){
+    case "red" :
+        console.log("STOP");
+        break ;
+    case "yellow" :
+        console.log("SLOW DOWN");
+        break ; 
+    case "green" :
+        console.log("GO");
+        break ;
+    default :
+    console.log("Broken light");               
+}
