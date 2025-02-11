5e690ccd7ea5b57896ceb4c19a044d2f169f0b83
ryan-4u <aaryanofficial64@gmail.com>
2025-02-12 00:33:39 +0530

    stting methods

---
+++ JavaScript/03/string methods/app.js
@@ -0,0 +1,28 @@
+// Trim method
+let msg = "   Hello   " ;
+console.log(msg);
+console.log(msg.trim());
+// toUpperCase and toLowerCase method
+let intro = "My name is Aaryan.";
+console.log(intro);
+console.log(intro.toLowerCase());
+console.log(intro.toUpperCase());
+// indexOf method
+let str = "ILoveCoding";
+console.log(str.indexOf("Love"));
+console.log(str.indexOf("j"));
+console.log(str.indexOf("e"));
+//method chaining
+let Greet = "      how are you ?         " ;
+let newGreet = Greet.trim().toUpperCase();
+console.log(newGreet);
+//slice method
+console.log(str.slice(5));
+console.log(str.slice(1.5));
+console.log(str.slice(-3));
+//replace method
+console.log(str.replace("Love","Do"));
+let name = "king" ;
+console.log(name.repeat(4));
+
+
