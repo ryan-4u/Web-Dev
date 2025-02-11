5d938f0dca882f42758a3f226960c5014f2b63b0
ryan-4u <aaryanofficial64@gmail.com>
2025-02-11 17:21:38 +0530

    logical operators practice ques

---
+++ JavaScript/02/operators/prac1.js
@@ -0,0 +1,14 @@
+//q1 : a string is a "good string"
+//     if it starts with'a' and
+//     has length>3 . WAP to find 
+//  if a string is good or not
+let name = 'aaryan' ;
+if( name[0]==='a' && name.length>3){
+    console.log("A good string");
+} else {
+    console.log("Not a good string");
+}
+
+
+//q2 : predicting output
+// ans : safe 
