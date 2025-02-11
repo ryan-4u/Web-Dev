0b59921e58b93d894b8e9d1ca5b91d100eaace45
ryan-4u <aaryanofficial64@gmail.com>
2025-02-11 15:29:58 +0530

    comparison operators

---
+++ JavaScript/02/operators/comparison.js
@@ -0,0 +1,12 @@
+let age =23 ;
+console.log(age<18); //false
+console.log(age>18);  //true
+age = 18 ;
+console.log(age<18);  //false
+console.log(age>18);  //false
+console.log(age<=18);  //true
+console.log(age>=18);  //true
+console.log(5==5);  //true
+console.log(7!=7);   //false
+
+console.log(a<b); //true
