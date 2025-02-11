5f42f05101b6285dc175a5e396a494fc2f316f63
ryan-4u <aaryanofficial64@gmail.com>
2025-02-11 18:07:21 +0530

    assigment q3

---
+++ JavaScript/02/assignment/q3.js
@@ -0,0 +1,17 @@
+let num = 7 ;
+switch(num){
+    case 1:
+        console.log("J,F,M");
+        break;
+        case 2:
+            console.log("A,M,J");
+            break;
+            case 3:
+                console.log("J,A,S");
+                break;
+                case 4:
+                    console.log("O,N,D");
+                    break;
+                    default :
+                    console.log("no case exist");
+}
