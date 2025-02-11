76f986474e90124d29051ad3725253cc914ecba3
ryan-4u <aaryanofficial64@gmail.com>
2025-02-11 16:03:53 +0530

    nested ifelse

---
+++ JavaScript/02/conditionals/nested.js
@@ -0,0 +1,11 @@
+let marks = 75 ;
+if (marks>33 ){
+    if(marks>80){
+        console.log("Outstanding");
+    }
+    else{
+        console.log("Excellent");
+    }
+} else {
+    console.log("Better luck next time.......");
+}
