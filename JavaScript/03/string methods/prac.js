bca9a2106493d0dec447dde9149c8a0f14437bc4
ryan-4u <aaryanofficial64@gmail.com>
2025-02-12 01:16:05 +0530

    stting methods pravtice ques

---
+++ JavaScript/03/string methods/prac.js
@@ -0,0 +1,11 @@
+//    q1
+let msg = "help!    ";
+console.log(msg.trim().toUpperCase());
+//    q2
+let name = "ApnaCollege" ;
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
+//    q3
+console.log(name.slice(4));
+console.log(name.slice(4).replace("l","t"));
