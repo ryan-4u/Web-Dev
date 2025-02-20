190b105651c0dc76951639b344d4e4ed2ae90057
ryan-4u <aaryanofficial64@gmail.com>
2025-02-20 10:31:48 +0530

    commiting

---
+++ JavaScript/03/Array/array.js
@@ -0,0 +1,11 @@
+let students = ["aman","amit","monu"];
+let nums = [2,4,6,8,10];
+//mixed array
+let info = ["aman",23,94];
+//empty array
+let arr =[];
+// arrays are immutable
+let fruits =["mango","apple","litchi"];
+fruits[0]="banana";
+//array methods
+// push unshift pop 
---
+++ JavaScript/03/string methods/array.js
@@ -0,0 +1,1 @@
+let students = ["aman","rahul","mohit","aryan"];
