b67a47992d7ecf14d1144ef07adf6d08cbfc0a0e
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 15:44:30 +0530

    creating and accessing object literal

---
+++ JavaScript/05/Object Literal/app.js
@@ -0,0 +1,23 @@
+const student = {
+    name :"aaryan" ,
+    age : 17 ,
+    marks : 82.4 
+};
+
+// Twitter post
+const post = {
+    username : "aaryan" ,
+    content :"health is wealth" ,
+    likes : 234 ,
+    repost : 17 ,
+    tags : ["@aman" , "@rahul" , "@monika"]
+};
+
+const obj = {
+    1 : 'a' ,
+    2 : 'b' ,
+    null : 'c' ,
+    true : 'd' ,
+    undefined : 'e'
+};
+
