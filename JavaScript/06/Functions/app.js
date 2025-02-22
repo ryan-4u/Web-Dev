6a52270b74da900c67572460e9ca71cc2cebd21c
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 21:31:15 +0530

    function with arguments

--- JavaScript/06/Functions/app.js
+++ JavaScript/06/Functions/app.js
@@ -1,5 +1,12 @@
 function hello() {
     console.log("hello ji !!");
 }
-hello();
-hello();
+
+// function with arguments
+function printName(name){
+    console.log(name);
+}
+//now with multiple arguments
+function printInfo(name,age,city){
+    console.log(`${name}'s age is ${age} and lives in ${city} ...`);
+}
