c0686ecc0c2fd500896997572f900cc294928389
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 10:29:08 +0530

    started loop and done odd even questions

---
+++ JavaScript/04/Loops/app.js
@@ -0,0 +1,21 @@
+for(let i=0 ; i<5 ;i++){
+    console.log(i);
+}
+//backward loop
+for(let i=10 ; i>=1 ;i=i-3){
+    console.log(i);
+}
+// printing odd numbers from 1 to 15
+console.log("odd number");
+for(let i=1 ; i<=15 ;i++){
+    if(i%2 != 0){
+        console.log(i);
+    }    
+}
+// printing even numbers from 1 to 15
+console.log("even number");
+for(let i=1 ; i<=15 ;i++){
+    if(i%2 == 0){
+        console.log(i);
+    }    
+}
