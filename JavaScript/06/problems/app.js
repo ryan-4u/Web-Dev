9708f1a768bfd65b031bedf998b28e66af1feab1
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 21:19:28 +0530

    function problem 1 & 2

---
+++ JavaScript/06/problems/app.js
@@ -0,0 +1,12 @@
+// practice question 02
+function rollDice(){
+    let num = Math.floor(Math.random()*6)+ 1;
+    console.log(num);
+}
+// practice question 01
+function printPoem() {
+    console.log("jhonny jhoony ... yes papa");
+    console.log("eating sugar ... no papa");
+    console.log("telling a lie ... no papa");
+    console.log("open your mouth ... ha ha ha");
+}
