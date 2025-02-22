daf640f7e8d61e05192f3ccf80ad74067441d434
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 10:46:31 +0530

    multiplication table

--- JavaScript/04/Loops/app.js
+++ JavaScript/04/Loops/app.js
@@ -1,21 +1,33 @@
-for(let i=0 ; i<5 ;i++){
-    console.log(i);
-}
-//backward loop
-for(let i=10 ; i>=1 ;i=i-3){
-    console.log(i);
-}
+// for(let i=0 ; i<5 ;i++){
+//     console.log(i);
+// }
+// //backward loop
+// for(let i=10 ; i>=1 ;i=i-3){
+//     console.log(i);
+// }
+// // printing odd numbers from 1 to 15
+// console.log("odd number");
+// for(let i=1 ; i<=15 ;i++){
+//     if(i%2 != 0){
+//         console.log(i);
+//     }    
+// }
+// // printing even numbers from 1 to 15
+// console.log("even number");
+// for(let i=1 ; i<=15 ;i++){
+//     if(i%2 == 0){
+//         console.log(i);
+//     }    
+// }
 // printing odd numbers from 1 to 15
-console.log("odd number");
-for(let i=1 ; i<=15 ;i++){
-    if(i%2 != 0){
-        console.log(i);
-    }    
-}
-// printing even numbers from 1 to 15
-console.log("even number");
-for(let i=1 ; i<=15 ;i++){
-    if(i%2 == 0){
-        console.log(i);
-    }    
-}
+// console.log("Multiplication Table of 5");
+// for(let i=5 ; i<=50 ;i=i+5){
+
+//         console.log(i);  
+// }
+// let n = prompt("write your number"); //prompt take in a integer
+// n = parseInt(n) ; //parseInt will select integer part of a string
+// for(let i=n ; i<=n*10 ; i = i+n){
+//     console.log(i);
+// }
+ 
