28c0cfc9027c09fa040d020ca7c7fb9edbdb0471
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 17:18:24 +0530

    nesting of object literal

--- JavaScript/05/Object Literal/app.js
+++ JavaScript/05/Object Literal/app.js
@@ -1,23 +1,44 @@
-const student = {
-    name :"aaryan" ,
-    age : 17 ,
-    marks : 82.4 
+// const student = {
+//     name :"aaryan" ,
+//     age : 17 ,
+//     marks : 82.4 
+// };
+
+// // Twitter post
+// const post = {
+//     username : "aaryan" ,
+//     content :"health is wealth" ,
+//     likes : 234 ,
+//     repost : 17 ,
+//     tags : ["@aman" , "@rahul" , "@monika"]
+// };
+
+// const obj = {
+//     1 : 'a' ,
+//     2 : 'b' ,
+//     null : 'c' ,
+//     true : 'd' ,
+//     undefined : 'e'
+// };
+
+let student = {
+    name : "aaryan" ,
+    age : 20 ,
+    marks : 82.4 ,
+    city : "Ambala"
 };
 
-// Twitter post
-const post = {
-    username : "aaryan" ,
-    content :"health is wealth" ,
-    likes : 234 ,
-    repost : 17 ,
-    tags : ["@aman" , "@rahul" , "@monika"]
+let classInfo = {
+    aman :{
+        grade : "A+",
+        city : "Delhi",
+    },
+    shradha : {
+        grade : "A+",
+        city : "Mumbai",
+    },
+    karan :{
+        grade : "A-",
+        city : "Haryana",
+    }
 };
-
-const obj = {
-    1 : 'a' ,
-    2 : 'b' ,
-    null : 'c' ,
-    true : 'd' ,
-    undefined : 'e'
-};
-
