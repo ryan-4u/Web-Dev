6c8ab0b8135365892e99ab257977abff3e8c945e
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 11:31:39 +0530

    break keyword in favorite movie game

--- JavaScript/04/Loops/favorite.js
+++ JavaScript/04/Loops/favorite.js
@@ -1,11 +1,13 @@
 console.log('Write "quit" to quit the game');
 const favorite = "spiderman";
 let guess = prompt("GUESS THE MOVIE");
-while((guess != favorite) && (guess != "quit")){
+while(guess != favorite){
+    if(guess == "quit"){
+        console.log("You have quit ");
+        break;
+    }
      guess = prompt("Wrong Guess , PLEASE TRY AGAIN !!");
 }
 if(guess == favorite){
     console.log("CONGRATS !! Your guess is RIGHT.");
-} else{
-    console.log("You have quit ");
-}
+}  
