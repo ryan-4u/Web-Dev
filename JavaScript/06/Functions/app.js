231fbb8518f418af39dba20e42f14f3b3e30aa0d
ryan-4u <aaryanofficial64@gmail.com>
2025-02-22 22:06:20 +0530

    return keyword

--- JavaScript/06/Functions/app.js
+++ JavaScript/06/Functions/app.js
@@ -10,3 +10,14 @@ function printName(name){
 function printInfo(name,age,city){
     console.log(`${name}'s age is ${age} and lives in ${city} ...`);
 }
+// return keyword
+function add(a,b) {
+    return a+b ;
+}
+function isAdult(age) {
+    if(age>=18){
+        return "adult";
+    } else {
+        return "not adult";
+    }
+}
