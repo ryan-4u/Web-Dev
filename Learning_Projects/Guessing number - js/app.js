8ef44e1a965edd8d416d85cda2406dbf12498bf8
Aaryan <aaryanofficial64@gmail.com>
2025-12-26 22:22:11 +0530

    Guess the number - mini project (only js)

--- Learning_Projects/Guessing number - js/app.js
+++ Learning_Projects/Guessing number - js/app.js
@@ -3,21 +3,21 @@ let random = Math.floor(Math.random() * max) + 1;
 
 let guess = 0;
 
-// while (true) {
-//     guess = prompt("Enter your guess between 1 and " + max);  
-//     if (guess == "exit") {
-//         alert("Game exited. The number was " + random);
-//         break;
-//     }
+while (true) {
+    guess = prompt("Enter your guess between 1 and " + max);  
+    if (guess == "exit") {
+        alert("Game exited. The number was " + random);
+        break;
+    }
 
-//     if (guess < random  ) {    
-//         alert("Too low! Try again.");
-//     } else if (guess > random) {
-//         alert("Too high! Try again.");
-//     } else if (guess == random) {
-//         alert("Congratulations! You guessed the number " + random + " correctly!");
-//         break;
-//     } else {
-//         alert("Invalid input. Please enter a number between 1 and " + max);
-//     }
-// }
+    if (guess < random  ) {    
+        alert("Too low! Try again.");
+    } else if (guess > random) {
+        alert("Too high! Try again.");
+    } else if (guess == random) {
+        alert("Congratulations! You guessed the number " + random + " correctly!");
+        break;
+    } else {
+        alert("Invalid input. Please enter a number between 1 and " + max);
+    }
+}
