d79523786de273ec010eb15c625de8b5a0e9654c
Aaryan <aaryanofficial64@gmail.com>
2026-01-02 16:10:05 +0530

    JS 03 arranged and revised

---
+++ JavaScript/03/assignment.js
@@ -0,0 +1,39 @@
+// Answers 
+// Ques 01 and Ques 02
+    // let arr = [7,9,0,-2] ;
+    // let n = 3 ;
+    // console.log("array:" ,arr) ;
+    // console.log("sliced array from start:" , arr.slice(0,n)) ; // ans 01
+    // console.log("sliced array from end:" , arr.slice(-n)) ;    // ans 02
+
+// Ques 03
+    // let str1 = "";
+    // let str2 = "Hello, World!" ;
+    // if( str2.length == 0){
+    //     console.log("Empty string");
+    // }else{
+    //     console.log("Not empty");
+    // }
+
+// Ques 04
+    // let str = "HelloWoRLd" ;
+    // let idx = 7 ;
+    // if( str[idx] == str[idx].toLowerCase() ){
+    //     console.log("Lowercase");
+    // }else{
+    //     console.log("Not Lowercase");
+    // }
+
+// Ques 05
+    // let str = prompt("please enter a string");
+    // console.log(`original string = ${str} `);
+    // console.log(`string without spaces = ${str.trim()}`);
+
+// Ques 06
+    let arr = [1,2,3,4,5] ;
+    let el = 3 ;
+    if(arr.indexOf(el) !== -1){
+        console.log("Element exists in array");
+    }else{
+        console.log("Element not exist in array");
+    }
