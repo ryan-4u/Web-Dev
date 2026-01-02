78490fe33759ae5efa45ba31823862382e748bb3
Aaryan <aaryanofficial64@gmail.com>
2026-01-02 14:10:04 +0530

    JS 02 arranged and revised

---
+++ JavaScript/02/assignment.js
@@ -0,0 +1,69 @@
+//----------------------------------------------------------------
+// Ques 01
+//let num = prompt("enter a number:");
+        // if (num % 10 == 0){
+        //     console.log("Good");
+        // }else{
+        //     console.log("Bad");
+        // }
+
+//----------------------------------------------------------------
+// Ques 02
+        // let name = prompt("Enter your name:");
+        // let age = prompt("Enter your age:");
+        // alert(`${name} is ${age} years old.`);
+
+//----------------------------------------------------------------
+// Ques 03
+        // let data = 3;
+        // switch(data){
+        //     case 1:
+        //         console.log("Jan", "Feb", "Mar");
+        //         break;  
+        //     case 2:
+        //         console.log("Apr", "May", "Jun");
+        //         break;  
+        //     case 3:
+        //         console.log("Jul", "Aug", "Sep");
+        //         break;  
+        //     case 4:
+        //         console.log("Oct", "Nov", "Dec");
+        //         break;  
+        //     default:
+        //         console.log("Invalid Input");
+        // }
+
+//----------------------------------------------------------------
+// Ques 04
+        // let str = prompt("Enter  a string:");
+        // if( (str.length>5) && (str[0]== 'a' || str[0] == 'A') ){
+        //     console.log("Golden String");
+        // }else{
+        //     console.log("Not a Golden String");
+        // }
+
+
+//----------------------------------------------------------------
+// ques 05
+        // let num1 = 8 ;
+        // let num2 = 12 ;
+        // let num3 = 5 ;
+        // if( (num1 >= num2) && (num1 >= num3) ){
+        //     console.log(`${num1} is the greatest number.`); 
+        // }else if( (num2 >= num1) && (num2 >= num3) ){
+        //     console.log(`${num2} is the greatest number.`); 
+        // }else{
+        //     console.log(`${num3} is the greatest number.`); 
+        // }   
+
+//----------------------------------------------------------------
+// Ques 06
+        // let number1 = 467 ;
+        // let number2 = 987897 ;
+        // a = number1 % 10 ;
+        // b = number2 % 10 ;
+        // if( a == b ){
+        //     console.log("Last digits are same.");
+        // }else{
+        //     console.log("Last digits are not same.");
+        // }
---
+++ JavaScript/02/linkingToHTML.js
@@ -0,0 +1,6 @@
+console.log("This side Aaryan");
+console.log("and I LOVE YOU");
+let a=5;
+let b=2;
+let output = `sum is ${a+b+b} rupees`;
+console.log(output);
